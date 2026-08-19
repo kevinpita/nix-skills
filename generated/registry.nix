01b7245ea4fd806512092dcda86718a4072be225
kevinpita <69063372+kevinpita@users.noreply.github.com>
2026-08-19 04:51:39 +0000

    Update skill handoff

--- generated/registry.nix
+++ generated/registry.nix
@@ -85,15 +85,15 @@ builtins.fromJSON ''
     ],
     "description": "Compact the current conversation into a handoff document for another agent to pick up.",
     "displayName": "Handoff",
-    "homepage": "https://github.com/mattpocock/skills/tree/068b6e0c62393147daf03530149cdce209c93da8/skills/productivity/handoff",
+    "homepage": "https://github.com/mattpocock/skills/tree/9c9f36ccd3995266cd675468af71639c8dde1ec5/skills/productivity/handoff",
     "license": "MIT",
     "slug": "handoff",
     "source": {
-      "narHash": "sha256-QYJScM5FqsN+QsHHo5oJx/1e7YqGxdod/Iv6kpnQZ+o=",
+      "narHash": "sha256-CJNC5fORkc+FGd+FlCXG6rZcVv2MCqCNHCVC0AW623Q=",
       "owner": "mattpocock",
       "path": "skills/productivity/handoff",
       "repo": "skills",
-      "rev": "068b6e0c62393147daf03530149cdce209c93da8",
+      "rev": "9c9f36ccd3995266cd675468af71639c8dde1ec5",
       "type": "github"
     },
     "tags": [
